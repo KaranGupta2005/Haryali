7fad4a6f7aa72aefdcf005d772607915fa28c8b7
KaranGupta_2006 <guptakaran.port@gmail.com>
2025-10-27 18:33:22 +0530

    schema validations

--- backend/src/middlewares/validate.js
+++ backend/src/middlewares/validate.js
@@ -1,5 +1,12 @@
-import userSchema from "../Schema";
+import userSchema from "../Schema.js";
+import ExpressError from "./expressError.js";
 
-export const validateUser = async(req,res,next)=>{
-    
-} 
+export const validateUser = (req, res, next) => {
+  const { error } = userSchema.validate(req.body);
+
+  if (error) {
+    throw new ExpressError(400, error.details[0].message);
+  } else {
+    next();
+  }
+};
